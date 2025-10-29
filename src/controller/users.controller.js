f30d0fb87aec66be1398b44cbfa19562d03725ce
Abhishek <abhishekkumargupta20112000@gmail.com>
2025-10-29 18:30:30 +0530

    limited fields selected

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -3,7 +3,7 @@ import User from "../model/user.model.js";
 export const fetchAllUsers = async (req, res, next) => {
     try {
         console.log("fetchAllUsers API..");
-        const allUsers = await User.find({});
+        const allUsers = await User.find({},'_id username email');
         console.log("allUsers>>",allUsers);
         res.status(200).json({
             message: "Fetched all Users",
